15eb37d2045a08bbb3ab84e0166f8d9fa6d4afa7
Maksim Aptishev <maximus.test.qa777@gmail.com>
2025-09-07 19:12:39 +0300

    Выполинили логику mainDo

--- src/main/java/com/stepup/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/com/stepup/proj/MainApplication.java
@@ -7,10 +7,11 @@ public class MainApplication {
         System.out.println("Hello World!");
         System.out.println("11111");
         System.out.println("222");
+        System.out.println("---");
     }
 
     public static void doSomething(){
         System.out.println("1");
-        System.out.println("2");
+        System.out.println("00");
     }
 }
