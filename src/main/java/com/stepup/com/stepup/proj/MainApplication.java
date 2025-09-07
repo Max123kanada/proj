5c2f6f4450ecae230b9033ecd2d87bb3798611a1
Maksim Aptishev <maximus.test.qa777@gmail.com>
2025-09-07 22:12:31 +0300

    изменили логику mainDo1/2

--- src/main/java/com/stepup/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/com/stepup/proj/MainApplication.java
@@ -4,13 +4,14 @@ import java.sql.SQLOutput;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("-----");
         System.out.println("Hello World!");
         System.out.println("11111");
         System.out.println("222");
     }
 
     public static void doSomething(){
-        System.out.println("1");
-        System.out.println("2");
+        System.out.println("8");
+        System.out.println("5");
     }
 }
